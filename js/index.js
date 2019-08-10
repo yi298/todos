2c9f8cd90c00e38c0df18dfeac4cf595f0182ebf
cyt <470922684@qq.com>
2019-08-10 11:54:25 +0800

    jQuery写法

--- js/index.js
+++ js/index.js
@@ -1,31 +1,50 @@
 // 获取按钮，设置点击事件
-var btn = document.getElementById("todoBtn");
-btn.onclick = function() {
+$("#todoBtn").click(function() {
   // 获取输入框的输入的内容
-  var todo = document.getElementById("todo").value.trim();
+  var $todo = $("#todo");
   // 验证非空
-  if (!todo) {
+  if (!$todo.val().trim()) {
     console.log("仅空格，不输出");
     return;
   }
-  // 获取时间
-  var date = new Date();
-  // 生成对应的标签（内容+时间），此时时间还是时间戳
-  var li =
-    "<li class='todo'><span>" +
-    new Date().fmt("yyyy-MM-dd hh:mm:ss") +
-    "</span>" +
-    todo +
-    "</li>";
-  // 获取文本框原有的内容
-  var todoes = document.getElementById("todoes");
-  // 把输入框新内容加进文本框
-  var html = todoes.innerHTML + li;
-  todoes.innerHTML = html;
+  var li = document.createElement("li"); //获取li标签
+  li.classList.add("list-group-item"); //添加标签
+  li.innerText = new Date().fmt("yyyy-MM-dd hh:mm:ss") + $todo.val(); //添加内容
+  $(".list-group").append(li);
   // 发送后，输入框置空
-  document.getElementById("todo").value = "";
-};
-
+  $todo.val("");
+  // //  把输入框新内容加进文本框
+  // var html = todoes.innerHTML + li;
+  // todoes.innerHTML = html;
+  // // 发送后，输入框置空
+  // document.getElementById("todo").value = "";
+});
+// btn.onclick = function() {
+// // 获取输入框的输入的内容
+// var todo = document.getElementById("todo").value.trim();
+// // 验证非空
+// if (!todo) {
+//   console.log("仅空格，不输出");
+//   return;
+// }
+// 获取时间
+// var date = new Date();
+// 生成对应的标签（内容+时间），此时时间还是时间戳
+// var li =
+//   "<li class='todo'><span>" +
+//   new Date().fmt("yyyy-MM-dd hh:mm:ss") +
+//   "</span>" +
+//   todo +
+//   "</li>";
+// 获取文本框原有的内容
+//   var todoes = document.getElementById("todoes");
+//   // 把输入框新内容加进文本框
+//   var html = todoes.innerHTML + li;
+//   todoes.innerHTML = html;
+//   // 发送后，输入框置空
+//   document.getElementById("todo").value = "";
+// };
+/*
 // 拥有进行时和完成时
 var ul = document.getElementById("todoes");
 // 事件委托  对li设置点击事件
@@ -43,6 +62,22 @@ ul.addEventListener("click", function(e) {
       e.target.classList.add("pedding");
       console.log("未完成 =》 进行时");
     }
+  }
+});
+*/
+
+$("#todoes").on("click", "li", function() {
+  console.log(this, "点击获取li标签");
+  var $li = $(this);
+  if ($li.hasClass("list-group-item-info")) {
+    $li.removeClass("list-group-item-info");
+    $li.addClass("list-group-item-success");
+  }
+  if (
+    !$li.hasClass("list-group-item-info") &&
+    !$li.hasClass("list-group-item-success")
+  ) {
+    $li.addClass("list-group-item-info");
   }
 });
 
@@ -76,7 +111,7 @@ Date.prototype.fmt = function(fmt) {
 String.prototype.trim = function() {
   return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, "");
 };
-
+/*
 function hasClass(array, className) {
   for (var i = 0; i < array.length; i++) {
     if (array[1] === className) {
@@ -85,3 +120,4 @@ function hasClass(array, className) {
     return false;
   }
 }
+*/
